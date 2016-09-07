e9d96af91dd2a04ffdcce3994967cdb5610802fa
Damon <damonnguyen92@gmail.com>
2016-09-07 15:06:32 -0700

    Feat: Create middleware for parsing JSON, URI, and serving static files

--- server/router.js
+++ server/router.js
@@ -1,2 +1,17 @@
 const express = require('express');
 const path = require('path');
+const app = express();
+const bodyParser = require('body-parser');
+
+//MIDDLEWARE:
+
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../assets')));
+
+app.get('/', function(req, res) {
+  console.log('logging on path /!');
+  res.sendFile(__dirname + '/assets/index.html');
+});
+
+module.exports = app;
