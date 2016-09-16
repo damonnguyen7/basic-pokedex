bab9c8c754d38f284e0b331513633b58d5708f30
Damon <damonnguyen92@gmail.com>
2016-09-16 13:06:18 -0700

    Feat: Set up configuration, state, and compile searchBar

--- public/app.js
+++ public/app.js
@@ -1,1 +1,19 @@
-var app = angular.module('Pokedex', []);
+angular.module('Pokedex', [
+  'SearchBar',
+  'ui.router',
+])
+
+.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/');
+  $stateProvider
+  .state('home', {
+    url: '/home',
+    templateUrl: './components/searchBar/searchBarView.html',
+    controller: './components/searchBar/searchBarController'
+  })
+}])
+.run(['$location', '$state', function ($location, $state) {
+  $location.url('/home');
+  $state.go('home');
+}]);
+
