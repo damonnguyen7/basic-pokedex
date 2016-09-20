efb3d51d7e3c1bb1e321f99706760c7dd3aa00b6
Damon <damonnguyen92@gmail.com>
2016-09-20 11:49:29 -0700

    Feat(init): Get data from pokeapi, filter data, and store multiple json data in array

--- server/models/pokemonDataModel.js
+++ server/models/pokemonDataModel.js
@@ -2,25 +2,50 @@ const request = require('request');
 const Promise = require('promise');
 
 var getPokemonData = function(pokemon) {
-
-  return function(res) {
-    return new Promise(function(resolve, reject) {
-      for (var i = 1; i < pokemon.length; i++) {
+  return new Promise(function(resolve, reject) {
+    var arrayOfPokemon = [];
+    for (var i = 1; i < pokemon.length; i++) {
+      (function(i){
         var url = 'http://pokeapi.co/api/v2/pokemon/' + i + '/';
         request(url, function(error, response, data) {
           if (error) {
             reject(error);
           } else {
-            //NEED TO FILTER DATA
-            resolve({
-              respond: res.send(JSON.parse(data)),
-              data: data
+            data = JSON.parse(data);
+            var pokemonObj = {};
+            pokemonObj.id = data.id;
+            pokemonObj.name = data.name[0].toUpperCase() + data.name.slice(1);
+            pokemonObj.height = data.height;
+            pokemonObj.weight = data.weight;
+            pokemonObj.stats = data.stats.map(function(statType) {
+              if (statType.stat.name === 'hp') {
+                return ['hp', statType.base_stat];
+              } else if (statType.stat.name === 'attack') {
+                return ['attack', statType.base_stat];
+              } else if (statType.stat.name === 'defense') {
+                return ['defense', statType.base_stat];
+              } else if (statType.stat.name === 'speed') {
+                return ['speed', statType.base_stat];
+              } else if (statType.stat.name === 'special-attack') {
+                return ['special-attack', statType.base_stat];
+              } else if (statType.stat.name === 'special-defense') {
+                return ['special-defense', statType.base_stat];
+              } 
             });
+            pokemonObj.moves = data.moves.map(function(moveObj) {
+              return moveObj.move.name;
+            });
+            arrayOfPokemon.push(pokemonObj);
+            if (arrayOfPokemon.length === 9) {
+              resolve({
+                data: {pokemonData: arrayOfPokemon}
+              });
+            }
           }
         });
-      }
-    })
-  }
+      })(i)
+    }
+  })
 }
 
 module.exports = getPokemonData;
