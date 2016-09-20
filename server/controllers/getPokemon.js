0c7e26c2112ebd3a3efa7e0017b4d054d33e4e7f
Damon <damonnguyen92@gmail.com>
2016-09-20 15:01:10 -0700

    Feat(init): Implement caching method with redis database

--- server/controllers/getPokemon.js
+++ server/controllers/getPokemon.js
@@ -1,17 +1,33 @@
 var getPokemon = require('../models/pokemonDataModel');
+var requireRedis = require('redis');
+var redis = requireRedis.createClient(6379, 'localhost');
 
 exports.post = function(req, res) {
   var listOfPokemon = req.body.data;
-  listOfPokemon = listOfPokemon.slice(0, 10);
+  var key = 'orginalPokemon';
 
-  //set header
-  res.setHeader('Content-Type', 'application/json');
+  // redis.del(key);
 
-  getPokemon(listOfPokemon)
-    .then(function(respond) {
-      console.log('successfully send data to client!');
-      res.send(respond.data);
+  redis.get(key, function(error, result) {
+    res.setHeader('Content-Type', 'application/json');
+    if (result) {
+      console.log('successfully retrieve from redis database');
+      console.log('data retrieved from redis database: ', JSON.parse(result).pokemonData.length);
+      res.send(JSON.parse(result));
       res.end();
-    });
-
+    } else {
+      getPokemon(listOfPokemon)
+        .then(function(respond) {
+          console.log('redis.set data in controller');
+          redis.set(key, JSON.stringify(respond.data));
+          redis.expire(key, 14400);
+          res.send(respond.data);
+          res.end();
+        })
+        .catch(function(error) {
+          res.setHeader('Content-Type', 'application/text');
+          res.status(500).send('Internal server error');
+        });
+    }
+  })
 }
