1f8abb0c5f9fe0912897d76f53f06d748d85a9e2
Damon <damonnguyen92@gmail.com>
2016-10-04 13:47:10 -0700

    Refactor: Use state provider to display multiple partial template

--- public/app.js
+++ public/app.js
@@ -1,30 +1,23 @@
-(function() {
-  'use strict';
-
-  angular.module('Pokedex', [
-    'Pokedex.List',
-    'Pokedex.ProfilePicture',
-    'ui.router',
-  ])
-
-  .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-    // $urlRouterProvider.otherwise('/');
-    $stateProvider
+angular.module('Pokedex', [
+  'Pokedex.List',
+  'Pokedex.ProfilePicture',
+  'ui.router',
+])
+.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/');
+  $stateProvider
     .state('home', {
-      url: '/home',
-      templateUrl: 'components/list/listView.html',
-      controller: 'ListController'
+      url: '/',
+      views: {
+        'list': {
+          templateUrl: 'components/list/listView.html',
+          controller: 'ListController'
+        },
+        'profilePicture': {
+          templateUrl: 'components/profilePicture/profilePicture.html',
+          controller: 'ProfilePicture'
+        }
+      }
     })
-    .state('home.selected', {
-      url: '/selected/:contactId',
-      templateUrl: 'components/profilePicture/profilePicture.html',
-      controller: 'ProfilePicture'
-    })
-  }])
-  .run(['$location', '$state', function ($location, $state) {
-    $location.url('/home');
-    $state.go('home');
-  }]);
+}])
   
-})();
-
