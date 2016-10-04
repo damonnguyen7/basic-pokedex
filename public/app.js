c54c064d8ce3b0edd44156c3c11e8aed9da761e0
Damon <damonnguyen92@gmail.com>
2016-10-04 10:37:38 -0700

    Feat: wrap main program in iife

--- public/app.js
+++ public/app.js
@@ -1,25 +1,30 @@
-angular.module('Pokedex', [
-  'List',
-  'ProfilePicture',
-  'ui.router',
-])
+(function() {
+  'use strict';
 
-.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise('/');
-  $stateProvider
-  .state('home', {
-    url: '/home',
-    templateUrl: 'components/list/listView.html',
-    controller: 'ListController'
-  })
-  .state('home.selected', {
-    url: '/home/{pokemonId}',
-    templateUrl: 'components/profilePicture/profilePicture.html',
-    controller: 'ProfilePicture'
-  })
-}])
-.run(['$location', '$state', function ($location, $state) {
-  $location.url('/home');
-  $state.go('home');
-}]);
+  angular.module('Pokedex', [
+    'Pokedex.List',
+    'Pokedex.ProfilePicture',
+    'ui.router',
+  ])
+
+  .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    // $urlRouterProvider.otherwise('/');
+    $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: 'components/list/listView.html',
+      controller: 'ListController'
+    })
+    .state('home.selected', {
+      url: '/selected/:contactId',
+      templateUrl: 'components/profilePicture/profilePicture.html',
+      controller: 'ProfilePicture'
+    })
+  }])
+  .run(['$location', '$state', function ($location, $state) {
+    $location.url('/home');
+    $state.go('home');
+  }]);
+  
+})();
 
